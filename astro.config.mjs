7f470ac0e706f52e3cfb016524b2c906288e8aed
hammaad-swe <hammaad.swe@gmail.com>
2025-06-12 16:07:52 +0530

    feat: v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   base: "/fast_prac",
+  site: "https://hammaadworks.github.io/fast_prac/",
   vite: {
       plugins: [tailwindcss()]
 	},
